c21e218ba13bdf13c6dabf0f9875d0687afe7e24
eslamkhaled201 <59029234+eslamkhaled201@users.noreply.github.com>
2020-11-16 16:26:00 +0200

    Updates

---
+++ precache-manifest.26f85a7aa71628dec5f8f3c6b17913cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e572a9330a29d2371df1cf537588cd09",
+    "url": "/react-project1/index.html"
+  },
+  {
+    "revision": "8e85b45d3d43f03a3a63",
+    "url": "/react-project1/static/css/main.5487f9f9.chunk.css"
+  },
+  {
+    "revision": "2b3b7f6fdce3c4257c1a",
+    "url": "/react-project1/static/js/2.e499a0ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-project1/static/js/2.e499a0ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e85b45d3d43f03a3a63",
+    "url": "/react-project1/static/js/main.cd227c84.chunk.js"
+  },
+  {
+    "revision": "433ec53e7f8d3d9b9b44",
+    "url": "/react-project1/static/js/runtime-main.ee3a51cb.js"
+  }
+]);
